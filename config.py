50b1fd99b90784a38e354ed5e81f138754ef1d74
Philippe <philippe@MacBook-Pro-de-Philippe.local>
2025-06-25 18:49:45 +0200

    Correction de main.py - parenthèse ligne 52

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 API_TOKEN = "SECURE_MY_TOKEN_123"
 
-QDRANT_HOST = "https://xxx.cloud.qdrant.io"
-QDRANT_API_KEY = "votre_cle_qdrant"
+QDRANT_HOST = "https://64ad2ddc-0cf6-49c7-9d1e-5e987213fbb1.europe-west3-0.gcp.cloud.qdrant.io:6333"
+QDRANT_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.nEaohrWLKKumvsdAeeMiQPk9AW06sVAiGRnXacSEAYY"
